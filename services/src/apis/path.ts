810b11429a412af14f85c9fcec87998578b72dcb
Nhan Nguyen <nhan.nguyen@katalon.com>
2024-05-27 23:45:16 +0700

    Add get github blogs

---
+++ services/src/apis/path.ts
@@ -0,0 +1,5 @@
+const API_V1 = '/api/v1';
+
+const GITHUB = `${API_V1}/github`;
+
+const GITHUB_BLOG = `${GITHUB}/blog`;
--- services/src/index.ts
+++ services/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import { fetchGitHubContent } from './apis/githubApis';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -17,7 +18,22 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
 
-
+app.get(GITHUB_BLOG, async (req: Request, res: Response) => {
+  try {
+    fetchGitHubContent('blogs').then((blogs) => {
+      res.send(blogs);
+    });
+  } catch (error: unknown) {
+    let message = 'Internal server error';
+    if (error instanceof Error) {
+      message = error.message;
+    } else if (typeof error === 'string') {
+      message = error;
+    }
+    console.error(error);
+    res.status(500).send(message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
