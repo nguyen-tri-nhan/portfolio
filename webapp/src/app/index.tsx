0e61b11103eb0e6fd6f37f59a0552271a14b252e
Nhan Nguyen <nhan.nguyen@katalon.com>
2024-05-29 22:26:31 +0700

    Use vite react instead of next (#2)

---
+++ webapp/src/app/index.tsx
@@ -0,0 +1,12 @@
+import style from './App.module.scss';
+
+function App() {
+
+  return (
+    <div className={style.appTest}>
+      hello world
+    </div>
+  )
+}
+
+export default App;
--- webapp/src/app/utils/crypto.ts
+++
@@ -1,4 +0,0 @@
-export function base64ToString(base64: string): string {
-    const decodedString = atob(base64);
-    return decodedString;
-}
