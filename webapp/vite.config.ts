06d7b2d5e4b7c05c2b0a793d896d8f1a36bdc1e0
Nhan Nguyen <nguyentrinhan.dev@gmail.com>
2026-06-21 11:57:45 +0700

    feat: Added java review

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'serve-static-html',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          const url = req.url?.split('?')[0]
+          if (url === '/review' || url === '/review/') {
+            const filePath = path.resolve(__dirname, 'public/review/index.html')
+            res.setHeader('Content-Type', 'text/html; charset=utf-8')
+            res.end(fs.readFileSync(filePath))
+            return
+          }
+          next()
+        })
+      },
+    },
+  ],
   server: {
     port: 3000,
   },
